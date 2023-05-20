e307f6fcfb719b64c72252bddd81f8f15cb14599
Sayon Usman <sayonusman.ma@gmail.com>
2023-05-20 15:46:28 +0300

    Added server connection to my toys page

--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -1,6 +1,23 @@
-import TableBody from "./TableBody/TableBody";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../providers/AuthContextProviders";
 
 const MyToys = () => {
+  const { user } = useContext(AuthContext);
+  const [myToys, setMyToys] = useState([]);
+  const [showToast, setShowToast] = useState(false);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/my_toys/${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.length === 0) {
+          setShowToast(true);
+        } else {
+          setMyToys(data);
+        }
+      });
+  }, [user?.email]);
+
   return (
     <div className="container mx-auto mt-10">
       <div className="overflow-x-auto">
@@ -10,16 +27,44 @@ const MyToys = () => {
               <th></th>
               <th>Seller Name</th>
               <th>Toy Name</th>
-              <th>Available Quantity</th>
+              <th>Available Qty</th>
               <th>Price</th>
               <th>Update Details</th>
               <th>Delete Toy</th>
             </tr>
           </thead>
 
-          <TableBody></TableBody>
+          {myToys &&
+            myToys.map((toy) => (
+              <tbody key={toy._id}>
+                <tr>
+                  <th></th>
+                  <td>{toy.sellerName}</td>
+                  <td>{toy.toyName}</td>
+                  <td>{`${toy.quantity} Dolls`}</td>
+                  <td>{`$${toy.price}`}</td>
+                  <td>
+                    <button className="link link-hover">Update</button>
+                  </td>
+
+                  <td>
+                    <button className="link link-hover">Delete</button>
+                  </td>
+                </tr>
+              </tbody>
+            ))}
         </table>
       </div>
+
+      {showToast && (
+        <div className="toast toast-end">
+          <div className="alert alert-info">
+            <div>
+              <span>You have no toy to view in the list.</span>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
