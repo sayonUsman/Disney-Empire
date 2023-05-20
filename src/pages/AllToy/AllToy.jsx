891cb7bc0cac1d6e2cfb1e8483fe60792d3a3fbd
Sayon Usman <sayonusman.ma@gmail.com>
2023-05-20 13:55:02 +0300

    Added server connection to all toy page

--- src/pages/AllToy/AllToy.jsx
+++ src/pages/AllToy/AllToy.jsx
@@ -1,8 +1,16 @@
-import TableBody from "./TableBody/TableBody";
+import { useEffect, useState } from "react";
 
 const AllToy = () => {
+  const [allToy, setAllToy] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/all_toy")
+      .then((res) => res.json())
+      .then((data) => setAllToy(data));
+  }, []);
+
   return (
-    <div className="container mx-auto mt-10">
+    <div className="container mx-auto mt-10 mb-5">
       <div className="overflow-x-auto">
         <table className="table table-compact w-full">
           <thead>
@@ -12,12 +20,26 @@ const AllToy = () => {
               <th>Toy Name</th>
               <th>Category</th>
               <th>Price</th>
-              <th>Available Quantity</th>
+              <th>Available Qty</th>
               <th>Details</th>
             </tr>
           </thead>
 
-          <TableBody></TableBody>
+          {allToy.map((toy) => (
+            <tbody key={toy._id}>
+              <tr>
+                <th></th>
+                <td>{toy.sellerName}</td>
+                <td>{toy.toyName}</td>
+                <td>{toy.category}</td>
+                <td>{`$${toy.price}`}</td>
+                <td>{`${toy.quantity} Dolls`}</td>
+                <td>
+                  <button className="link link-hover">View Details</button>
+                </td>
+              </tr>
+            </tbody>
+          ))}
         </table>
       </div>
     </div>
