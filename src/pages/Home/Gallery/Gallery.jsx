b36147492163b90ed3d0c6098962ee9f0a9f4721
Sayon Usman <sayonusman.ma@gmail.com>
2023-05-20 21:49:04 +0300

    Added gallery section in home page

---
+++ src/pages/Home/Gallery/Gallery.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+const Gallery = () => {
+  const [exhibitionToys, setExhibitionToys] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then((res) => res.json())
+      .then((data) => setExhibitionToys(data));
+  }, []);
+
+  return (
+    <>
+      {exhibitionToys.map((toy) => (
+        <div className="flex mx-auto" key={toy._id}>
+          <div className="card rounded-lg w-96 shadow-lg shadow-pink-500 mt-5">
+            <figure className="px-10 pt-10">
+              <img
+                src={toy.image}
+                alt="Doll Image"
+                className="h-96 rounded-lg"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">{toy.short_description}</h2>
+
+              <div className="card-actions">
+                <button className="btn rounded-md bg-info text-black hover:text-white mt-3">
+                  Buy Now
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Gallery;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,18 @@
+import Gallery from "./Gallery/Gallery";
 import Header from "./Header/Header";
 
 const Home = () => {
   return (
-    <div>
+    <div className="container mx-auto mb-10">
       <Header></Header>
+
+      <div className=" bg-gradient-to-t from-white to-pink-200 pt-3 pb-3 text-center text-xl lg:text-3xl font-semibold md:mb-3 rounded">
+        Gallery
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <Gallery></Gallery>
+      </div>
     </div>
   );
 };
